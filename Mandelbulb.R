0b1268e78d0261bee5d884adbbe5027004d0e3a6
Hao <32727671+HaoLi111@users.noreply.github.com>
2020-05-09 00:51:57 -0700

    Add files via upload

--- Mandelbulb.R
+++ Mandelbulb.R
@@ -12,51 +12,65 @@ getDoParWorkers()
 setwd("D:/HypercomplexFractal")
 zbase <- ybase <- xbase <- seq(from=-2,to=2,by=.2)# LOL
 #http://www.bugman123.com/Hypercomplex/index.html
-"%HC_power%"<-function(x,n){
+HC_power<-function(x,n){
   r = sqrt(sum(x^2))
+  if(r==0) r=+2.220446e-16
   y = x[2];z=x[3];x=x[1]
-  theta = n * atan(y/x);f = n*asin(z/r)
+  if(x==0) x=+2.220446e-16
+  #if(z==0) z=+2.220447e-16
+  theta = n * atan2(y,x);f = n*asin(z/r)
   return(r^n*c(cos(theta)*cos(f),sin(theta)*cos(f),-sin(f)))
 }
 
 escape_General_Mandelbulb = function(x,n=2,maxIter = 200){
+
   C = x
   for(i in 1:maxIter){
-    x = fi[p]x<- x%HC_power%n + C
-    if(sqrt(sum(x))>2){
+    x = HC_power(x,n)+ C
+    #print("work")
+    if(sqrt(sum(x^2))>2){
       n_escape = i
       return(n_escape)
     }
   }
+
   return(maxIter+1)
 }
+escape_General_Mandelbulb(c(0,.1,.2))
 
 
 
 
-
+system.time({
+  
 n = 2
 
 
 volumetric = list()
-for(iz in seq_along(z)){
+for(iz in seq_along(zbase)){
   z = zbase[iz]
-  m<-foreach(y=ybase,.combine = cbind) %dopar% {
+  print('Working on z = ')
+  print(z)
+  m<-foreach(iy=seq_along(ybase),.combine = cbind,.export =c("HC_power","escape_General_Mandelbulb")) %dopar%  {
+    
     v=numeric(length(ybase))
-    for(x in seq_along(xbase)){
-      xn = c(xbase[x],y,z)
-      v[x]= escape_General_Mandelbulb(xn,n)
+    for(ix in seq_along(xbase)){
+      xn = c(xbase[ix],ybase[iy],z)
+      v[ix]= escape_General_Mandelbulb(xn,n)#+sum(xn)
     }
-    v
+    v       
   }
   volumetric[[iz]]=m
 }
+})
+#escape_General_Mandelbulb(c(.1,.1,.1))
 
+system.time({
+  volumetric=abind(volumetric,along = 3)
+  saveRDS(volumetric,"Mandelbulb.RDS")
+  
+})
 
-
-
-volumetric=abind(volumetric,m,along = 3)
-saveRDS(volumetric,"Mandelbulb.RDS")
 
 #install.packages("plot3D")
 #install.packages("misc3d")
